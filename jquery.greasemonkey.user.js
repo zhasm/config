7e60be43698570db6f3d474ec0a6d359bc4c001b
rex <rex@zhasm.com>
2010-09-18 21:17:07 +0800

    jquery grease monkey template added

---
+++ jquery.greasemonkey.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name           
+// @description    
+// @copyright      
+// @license        GPL version 3 or later; http://www.gnu.org/copyleft/gpl.html
+// @include        
+// @namespace      Rex Zhang @ TOC  Shanghai 
+// @version        
+// history:         
+
+// ==/UserScript==
+
+
+
+
+
+//loading external js;
+//the jQuery library is same with wanghui's;
+function loadJS(src, element){ 
+
+    if (element == null) element = "head";
+    var GM_JQ = document.createElement('script');
+    GM_JQ.src = src;
+    GM_JQ.type = 'text/javascript';
+    document.getElementsByTagName(element)[0].appendChild(GM_JQ);
+
+}
+
+
+//the global code begins;
+//Add jQuery
+loadJS('http://code.jquery.com/jquery-1.4.2.min.js'); 
+
+
+// Check if jQuery's loaded
+function GM_wait() {
+    if(typeof unsafeWindow.jQuery == 'undefined') { 
+        window.setTimeout(GM_wait,100); 
+    }
+    else { $ = unsafeWindow.jQuery; letsJQuery(); }
+}
+
+GM_wait();
+
+// All your GM code must be inside this function
+function letsJQuery() {
+   $.noConflict();
+   //start your codes here
+   
+} 
